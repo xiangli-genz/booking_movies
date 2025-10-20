16290404a3a324c634367debb4b0079517c65d29
Xiang Li <snow03t6@gmail.com>
2025-10-21 02:33:14 +0700

    thêm 1 tí

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -13,3 +13,13 @@ module.exports.forgotPassword = async (req, res) => {
         pageTitle: "Quên mật khẩu"
     })
 }
+module.exports.otpPassword = async (req, res) => {
+    res.render("admin/pages/otp-password", {
+        pageTitle: "Xác thực OTP"
+    })
+}
+module.exports.resetPassword = async (req, res) => {
+    res.render("admin/pages/reset-password", {
+        pageTitle: "Đặt lại mật khẩu"
+    })
+}
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -8,4 +8,8 @@ router.get("/register", accountController.register)
 
 router.get("/forgot-password", accountController.forgotPassword)
 
+router.get("/otp-password", accountController.otpPassword)
+
+router.get("/reset-password", accountController.resetPassword)
+
 module.exports = router;
