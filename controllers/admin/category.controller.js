c6f694e7623f4eb194f241a37cc16109da737d43
Xiang Li <snow03t6@gmail.com>
2025-10-23 18:03:33 +0700

    đẩy ảnh lên đám mây

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -20,6 +20,7 @@ module.exports.createPost = async (req, res) => {
 
   req.body.createdBy = req.account.id;
   req.body.updatedBy = req.account.id;
+  req.body.avatar = req.file ? req.file.path : "";
 
   const newRecord = new Category(req.body);
   await newRecord.save();
--- routes/admin/category.route.js
+++ routes/admin/category.route.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const multer  = require('multer');
-const upload = multer();
-
 
 const categoryController = require("../../controllers/admin/category.controller");
+
+const cloudinaryHelper = require("../../helpers/cloudinary.helper");
+
+const upload = multer({ storage: cloudinaryHelper.storage });
 
 router.get("/list", categoryController.list);
 
