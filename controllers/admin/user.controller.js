e751addd06a997a5a4e83d7a7681feb417f8c37e
Xiang Li <snow03t6@gmail.com>
2025-12-02 01:35:50 +0700

    thêm tạm đặt vé

--- controllers/admin/user.controller.js
+++ controllers/admin/user.controller.js
@@ -1,5 +1,134 @@
+const moment = require("moment");
+const User = require("../../models/user.model");
+
 module.exports.list = async (req, res) => {
-    res.render("admin/pages/user-list", {
-        pageTitle: "Quản lý người dùng"
+  const find = {
+    deleted: false
+  };
+
+  // Lọc theo trạng thái
+  if(req.query.status) {
+    find.status = req.query.status;
+  }
+
+  // Lọc theo ngày tạo
+  const dateFilter = {};
+
+  if(req.query.startDate) {
+    const startDate = moment(req.query.startDate).startOf("date").toDate();
+    dateFilter.$gte = startDate;
+  }
+
+  if(req.query.endDate) {
+    const endDate = moment(req.query.endDate).endOf("date").toDate();
+    dateFilter.$lte = endDate;
+  }
+
+  if(Object.keys(dateFilter).length > 0) {
+    find.createdAt = dateFilter;
+  }
+
+  // Tìm kiếm
+  if(req.query.keyword) {
+    const keywordRegex = new RegExp(req.query.keyword, "i");
+    find.$or = [
+      { fullName: keywordRegex },
+      { email: keywordRegex },
+      { phone: keywordRegex }
+    ];
+  }
+
+  const userList = await User
+    .find(find)
+    .sort({
+      createdAt: "desc"
     });
+
+  for (const item of userList) {
+    item.createdAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY");
+  }
+
+  res.render("admin/pages/user-list", {
+    pageTitle: "Quản lý người dùng",
+    userList: userList
+  });
+}
+
+module.exports.changeStatusPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const status = req.body.status;
+
+    await User.updateOne({
+      _id: id,
+      deleted: false
+    }, {
+      status: status
+    });
+
+    req.flash("success", "Đổi trạng thái thành công!");
+
+    res.json({
+      code: "success"
+    });
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Id không hợp lệ!"
+    });
+  }
+}
+
+module.exports.deletePatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+    
+    await User.deleteOne({
+      _id: id
+    });
+
+    req.flash("success", "Xóa người dùng thành công!");
+
+    res.json({
+      code: "success"
+    });
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Id không hợp lệ!"
+    });
+  }
+}
+
+module.exports.changeMultiPatch = async (req, res) => {
+  try {
+    const { option, ids } = req.body;
+
+    switch (option) {
+      case "active":
+      case "inactive":
+        await User.updateMany({
+          _id: { $in: ids }
+        }, {
+          status: option
+        });
+        req.flash("success", "Đổi trạng thái thành công!");
+        break;
+      case "delete":
+        await User.deleteMany({
+          _id: { $in: ids }
+        });
+        req.flash("success", "Xóa thành công!");
+        break;
+    }
+
+    res.json({
+      code: "success"
+    });
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Id không tồn tại trong hệ thống!"
+    });
+  }
 }
--- routes/admin/user.route.js
+++ routes/admin/user.route.js
@@ -4,4 +4,10 @@ const userController = require("../../controllers/admin/user.controller");
 
 router.get("/list", userController.list);
 
+router.patch('/change-status/:id', userController.changeStatusPatch);
+
+router.patch('/delete/:id', userController.deletePatch);
+
+router.patch('/change-multi', userController.changeMultiPatch);
+
 module.exports = router;
